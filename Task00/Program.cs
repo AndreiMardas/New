b3cd33fdc88a647975f0ccc348e4a62b714fffef
Andrei Mardas <andrei.mardas.95@gmail.com>
2023-04-04 02:16:23 +0700

    task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int square = numbers * numbers;
+Console.WriteLine ($"Квадрат числа {numbers} = {square}");
