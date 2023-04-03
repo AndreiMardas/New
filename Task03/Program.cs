4392ee33c0bade97af63a2dc40aca2ba99d30751
Andrei Mardas <andrei.mardas.95@gmail.com>
2023-04-04 02:48:58 +0700

    task03

---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число: ");
+int oneNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int twoNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(oneNumber > max)
+{
+    max = oneNumber;
+}
+if(twoNumber > max)
+{
+    max = twoNumber;
+}
+if(threeNumber > max)
+{
+    max = threeNumber;
+}
+
+Console.WriteLine($"max = {max}");
