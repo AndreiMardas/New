e1f240a7a1e27d7155fed8fa24a7146f2c436e16
Andrei Mardas <andrei.mardas.95@gmail.com>
2023-04-04 02:56:48 +0700

    task05

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
+else
+{
+Console.WriteLine("Введено ненатуральное число");
+}
